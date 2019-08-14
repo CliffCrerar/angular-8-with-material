247903e56251185a66f881c997bf9913079a95d0
Cliff <Crerar>
2019-08-14 13:32:04 +0200

    first commi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Angular material 
+
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+// Uncomment to turn off animations
+// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+
+  BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    /*NoopAnimationsModule | remove this comment to remove animaiton | */
   ],
   providers: [],
   bootstrap: [AppComponent]
